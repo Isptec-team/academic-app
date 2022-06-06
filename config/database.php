87dbf84066fbfd0b19a6274e059eccf78edd19a1
Kuenda Love <kuendalove14@gmail.com>
2022-06-06 10:57:16 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Str
+use Illuminate\Support\Str;
     
     $DATABASE_URL = parse_url('postgres://ggyvleunnbmaql:298646da7aecff2113024496d116e96b1672a587705580c5158eeedb331d8111@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d4tuf8fh7d3jvr');
 
