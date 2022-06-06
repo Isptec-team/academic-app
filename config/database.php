605954832ed3877535f12f3963e8d64bfc805c91
Kuenda Love <kuendalove14@gmail.com>
2022-06-06 10:59:14 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => $DATABASE_URL = parse_url('postgres://ggyvleunnbmaql:298646da7aecff2113024496d116e96b1672a587705580c5158eeedb331d8111@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d4tuf8fh7d3jvr');
+            'url' => $DATABASE_URL = parse_url('postgres://ggyvleunnbmaql:298646da7aecff2113024496d116e96b1672a587705580c5158eeedb331d8111@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d4tuf8fh7d3jvr'),
             'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
